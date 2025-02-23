f9b00df264993de33261813abefdf8258de35891
Azimjonov Islomjon <azimjonovislomjon77@gmail.com>
2025-02-23 23:24:13 +0500

    first commit

---
+++ shop/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from shop import views
+
+urlpatterns = [
+    path('home/', views.index, name='index'),
+    path('product-detail/<int:product_id>/', views.product_detail, name='product_detail'),
+]
